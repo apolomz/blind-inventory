c9a27c69e2a05b62ca1ab1f059122f882fa5abd0
Jhoan Fernandez <fernandez.jhoan@correunivalle.edu.co>
2026-09-01 20:49:42 -0500

    add README

--- Backend/main.py
+++ Backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="BlindInventory POS API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Permite que React se conecte
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
